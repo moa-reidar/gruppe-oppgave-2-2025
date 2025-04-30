186e20fe37bb6c3f0887cff788b6975b37259ea7
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-30 19:02:30 +0200

    Set up main layout in App component using core components

--- plant-care-app/src/App.jsx
+++ plant-care-app/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'; 
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './Components/Header'; 
+import Footer from './components/Footer';
+import SearchBar from './components/SearchBar';
+import FilterSortbar from './components/FilterSortBar'; 
+import Plantlist from './components/PlantList'; 
+import AddPlantForm from './components/AddPlantForm';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div> 
+      <Header />
+      <main>
+        <SearchBar />
+        <FilterSortbar />
+        <Plantlist />
+        <AddPlantForm />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default app; 
