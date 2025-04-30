137563be07258824db77294df26f86a801316c16
omar <omarfarah2002@hotmail.com>
2025-04-30 23:22:16 +0200

    Set up AddPlantForm

--- plant-care-app/src/components/AddPlantForm.jsx
+++ plant-care-app/src/components/AddPlantForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddPlantForm = () => {
+  const [name, setname] = useState('');
+  const [image, setimage] = useState('');
+
   return (
-    <div>AddPlantForm</div>
+    <form>
+      <input
+        type="text"
+        placeholder="Plant Name"
+        value={name}
+        onChange={ setName(target.value)}
+      />
+      <input
+        type="text"
+        placeholder= "URL"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+      <button type="submit">Add Plant</button>
+    </form>
   );
 };
 
-export default AddPlantForm;
+export default addPlantForm;
