7004036e621f68020bb8baf6d3b092ec09d88699
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-30 19:12:36 +0200

    Fix component imports and naming issues in App.jsx

--- plant-care-app/src/App.jsx
+++ plant-care-app/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'; 
+import React from 'react';
 
-import Header from './Components/Header'; 
+import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
-import FilterSortbar from './components/FilterSortBar'; 
-import Plantlist from './components/PlantList'; 
+import FilterSortBar from './components/FilterSortBar';
+import PlantList from './components/PlantList';
 import AddPlantForm from './components/AddPlantForm';
 
 const App = () => {
   return (
-    <div> 
+    <>
       <Header />
       <main>
         <SearchBar />
-        <FilterSortbar />
-        <Plantlist />
+        <FilterSortBar />
+        <PlantList />
         <AddPlantForm />
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
-export default app; 
+export default App;
