b440f955908d66d41b4e3b2aad83d9e3749ab898
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-30 18:32:42 +0200

    Add base structure for all components

---
+++ plant-care-app/src/components/FilterSortBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FilterSortBar = () => {
+  return (
+    <div>FilterSortBar</div>
+  );
+};
+
+export default FilterSortBar;
---
+++ plant-care-app/src/components/PlantDetailModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlantDetailModal = () => {
+  return (
+    <div>PlantDetailModal</div>
+  );
+};
+
+export default PlantDetailModal;
