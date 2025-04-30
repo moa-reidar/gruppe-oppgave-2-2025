2dbd19c7302baa661fe20a3919cbf48e392abfb7
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-30 19:48:13 +0200

    Add dummy plant data using useState and pass to PlantList as props

--- plant-care-app/src/App.jsx
+++ plant-care-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,13 +8,31 @@ import PlantList from './components/PlantList';
 import AddPlantForm from './components/AddPlantForm';
 
 const App = () => {
+  const [plants, setPlants] = useState([
+    {
+      id: 1,
+      name: "Snake Plant",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      id: 2,
+      name: "Monstera",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      name: "Aloe Vera",
+      image: "https://via.placeholder.com/150",
+    }
+  ]);
+
   return (
     <>
       <Header />
       <main>
         <SearchBar />
         <FilterSortBar />
-        <PlantList />
+        <PlantList plants={plants} />
         <AddPlantForm />
       </main>
       <Footer />
@@ -23,3 +41,4 @@ const App = () => {
 };
 
 export default App;
+
