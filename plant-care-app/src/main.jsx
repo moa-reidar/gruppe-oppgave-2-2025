02d1f9356748aa91f447b9f3797b5f1b874da147
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-05-06 13:14:06 +0200

    chenged name and the links from style to css

--- plant-care-app/src/main.jsx
+++ plant-care-app/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/reset.css';
-import './styles/variables.css';
-import './styles/style.css';
+import './css/reset.css';
+import './css/variables.css';
+import './css/style.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
